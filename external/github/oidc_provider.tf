99ddc630ba98e33e7102a847f21ad6a439b00ed8
alpha3002025 <alpha300uk@gmail.com>
2026-02-12 17:18:35 +0900

    chore: removed unused lines

--- external/github/oidc_provider.tf
+++ external/github/oidc_provider.tf
@@ -9,5 +9,3 @@ resource "aws_iam_openid_connect_provider" "github" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
-
-
